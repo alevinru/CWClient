094ed1667246e5d34b8951c7762f32f5b9b383b0
Alexander Levin <alevin.ru@gmail.com>
2018-08-13 16:31:20 +0300

    Update redis.js

--- src/db/redis.js
+++ src/db/redis.js
@@ -8,9 +8,6 @@ export const hsetAsync = promisify(client.hset).bind(client);
 
 const debug = require('debug')('laa:cwc:redis');
 
-// if you'd like to select database 3, instead of 0 (default), call
-// client.select(3, function() { /* ... */ });
-
 client.on('error', err => {
   debug('Error', err);
 });
